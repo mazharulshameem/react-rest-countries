adc0197bbd347b6945b33073e3d77e4894cf62b2
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-09-21 19:44:45 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Countries></Countries>
+      <Footer></Footer>
 
 
     </div>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <p> Copyright All Right Reversed || 2022</p>
+    </div>
+  );
+};
+
+export default Footer;
